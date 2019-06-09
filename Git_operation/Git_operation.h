07f1e2751658841809772ce590b968a1a4e784e8
asdkj54 <1450438214@qq.com>
2019-06-09 16:41:32 +0800

    添加项目文件。

---
+++ Git_operation/Git_operation.h
@@ -0,0 +1,32 @@
+﻿
+// Git_operation.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CGitoperationApp:
+// 有关此类的实现，请参阅 Git_operation.cpp
+//
+
+class CGitoperationApp : public CWinApp
+{
+public:
+	CGitoperationApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CGitoperationApp theApp;
